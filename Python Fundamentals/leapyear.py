64e8551bb9a392e430b0102c04750243c283267d
Erwin Furth <e.furth@xs4all.nl>
2022-12-14 12:46:33 +0100

    1.3 with comments

--- Python Fundamentals/leapyear.py
+++ Python Fundamentals/leapyear.py
@@ -1,7 +1,20 @@
 '''is this a leapyear?'''
-input_year = input('Year: ')
-year = int(input_year)
 
-is_leapyear = year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+# Ask the user to input a year.
+input_year: str = input('Year: ')
 
+# Change the input to a number using int()
+year: int = int(input_year)
+
+# Calculate if the year is a leapyear
+#   1. a year is a leapyear if the year can be divided by 4
+#   2. but ( and ) the year can not be divided by 100
+#   3. except ( or ) if the year can be divided by 400
+can_be_divided_by_4: bool = year % 4 == 0
+can_not_be_divided_by_100: bool = year % 100 != 0
+can_be_divided_by_400: bool = year % 400 == 0
+
+is_leapyear: bool = can_be_divided_by_4 and can_not_be_divided_by_100 or can_be_divided_by_400
+
+# Print the result
 print(year, " is a leap year : ", is_leapyear)
