ee72c2b3cf1d8e6e12c79b114fd80f4c2d54b160
Erwin Furth <e.furth@xs4all.nl>
2022-12-14 15:19:47 +0100

    1.6 print statements follow case; new example

--- Python Fundamentals/strings.py
+++ Python Fundamentals/strings.py
@@ -4,12 +4,12 @@
 t: str = input("Input some text: ")
 
 # Print the tekst in all uppercase and also in all lowercase characters
-print("UPPER:", t.upper())
-print("lower:", t.lower())
+print("TEXT IN UPPER:", t.upper())
+print("text in lower:", t.lower())
 
 # Use the capitalize() and title() methods and print the results
-print("Capitalize:", t.capitalize())
-print("title:", t.title())
+print("Capitalized text:", t.capitalize())
+print("Text In Title:", t.title())
 
 # Print the first three characters by using slicing
 print("First three characters:", t[:3])
@@ -19,15 +19,15 @@ print("Ends with '?' :", t[-1] == '?')
 
 # Print the text in lowercase with all spaces replaced by an underscore
 # by using the method replace(). This is called snake_case
-print("snake_case:", t.lower().replace(' ', '_'))
+print("text_in_snake_case:", t.lower().replace(' ', '_'))
 
 # Output:
 # > python .\strings.py
-# Input some text: this is THE TEXT for ExPeRiMeNtInG With Strings.
-# upper: THIS IS THE TEXT FOR EXPERIMENTING WITH STRINGS.
-# lower: this is the text for experimenting with strings.
-# capitalize: This is the text for experimenting with strings.
-# title: This Is The Text For Experimenting With Strings.
+# Input some text: this is THE TEXT for ExPeRiMeNtInG With Strings?
+# TEXT IN UPPER: THIS IS THE TEXT FOR EXPERIMENTING WITH STRINGS?
+# text in lower: this is the text for experimenting with strings?
+# Capitalized text: This is the text for experimenting with strings?
+# Text In Title: This Is The Text For Experimenting With Strings?
 # First three characters: thi
-# Ends with '?': False
-# snake_case: this_is_THE_TEXT_for_ExPeRiMeNtInG_With_Strings.
+# Ends with '?': True
+# text_in_snake_case: this_is_the_text_for_experimenting_with_strings?
