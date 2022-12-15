852e50b6ea79aad25e8db7a98064e53ad1c3d9a4
Erwin Furth <e.furth@xs4all.nl>
2022-12-15 12:54:17 +0100

    2.4 cards

--- Python Fundamentals/cards.py
+++ Python Fundamentals/cards.py
@@ -2,11 +2,20 @@
 import random
 # Select 5 random cards from a deck of playing cards.
 
+# Suits (Unicode symbols) and Ranks
 suits: list[str] = ['\u2663', '\u2666', '\u2665', '\u2660']
 ranks: list[str] = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',')
+
+# All combinations of ranks and suits
 cards: list[str] = [r + s for r in ranks for s in suits]
+
+# Shuffle the deck
 random.shuffle(cards)
+
+# Deal the top 5 cards
 hand: list[str] = [cards.pop() for _ in range(5)]
+
+# Print the result
 print(' '.join(hand))
 
 # Output:
