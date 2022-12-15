70dc824647db2240482ad3025c34926d8c5bb4d3
Erwin Furth <e.furth@xs4all.nl>
2022-12-15 12:35:40 +0100

    2.3 passwords

---
+++ Python Fundamentals/passwords.py
@@ -0,0 +1,17 @@
+'''2.3: Password generator'''
+import random
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+
+# Generate a password of at least 6 characters
+# with at least 1 capital, 1 lowercase, 1 number and 1 special character.
+
+part1: list[str] = random.choices(ascii_lowercase, k=3)
+part2: list[str] = random.choices(ascii_uppercase, k=3)
+part3: list[str] = random.choices(digits, k=3)
+part4: list[str] = random.choices(punctuation, k=3)
+
+characters: list[str] = part1 + part2 + part3 + part4
+random.shuffle(characters)
+password: str = ''.join(characters)
+
+print(password)
